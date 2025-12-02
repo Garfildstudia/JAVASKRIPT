3d65cdd7811327a9daaa72d051d353ac78a78f94
Garfildstudia <piter100246@gmail.com>
2025-12-02 23:35:10 +0200

    end

---
+++ HW_2_JS/hw_2_JS.js
@@ -0,0 +1,25 @@
+//#awLXL6TBzg
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+
+if (coursesAndDurationArray[0].monthDuration > 5) {
+    console.log(`JavaScript Complex - SUPER`);
+} if (coursesAndDurationArray[1].monthDuration > 5){
+    console.log(`Java Complex- SUPER`);
+} if (coursesAndDurationArray[2].monthDuration > 5) {
+    console.log(`Python Complex-SUPER`);
+} if (coursesAndDurationArray[3].monthDuration > 5) {
+    console.log(`QA Complex-SUPER`);
+} if (coursesAndDurationArray[4].monthDuration > 5) {
+    console.log(`FullStack-SUPER`);
+} if (coursesAndDurationArray[5].monthDuration > 5) {
+    console.log(`Frontend-SUPER`);
+}
