1628f714fd3789eee5cc19f045dcbe388391b233
Garfildstudia <piter100246@gmail.com>
2025-12-11 22:08:02 +0200

    end

---
+++ HW_3_js_DOP_DZ/4aDbSgh/4aDbSgh.js
@@ -0,0 +1,12 @@
+//#4aDbSgh
+//— Створити пустий масив. Наповнити його будь-якими значеннями, звертаючись до конкретного індексу. Вивести в консоль
+
+let massivv = [];
+
+
+massivv[0] = true;
+massivv[1] = `yyy`;
+massivv[2] = `yyg`;
+massivv[3] = false;
+massivv[4] = 10;
+console.log(massivv);
